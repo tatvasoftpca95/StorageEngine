a78795011fa89d546c3dee5db55c1c3bb3b1bfc9
tatvasoftpca95 <mohit.kumar@tatvasoft>
2024-03-20 14:20:16 +0530

    code after adding liquibase and data

---
+++ src/main/resources/db/changelog/insert.sql
@@ -0,0 +1,4 @@
+<changeSet id="1" author="mohit">
+<sql>INSERT INTO timetable.academic_details(academic_id, duration_month, end_date, start_date, academic_name) VALUES (3, 3, '2024-11-06', '2024-08-06', 'semester3');</sql>
+</changeSet>
+
